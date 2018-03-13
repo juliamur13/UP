8d5120ed20946442719ac0857db64051e826c21b
Julia Murashko <my_dream13@list.ru>
2018-03-13 16:41:08 +0300

    task5

--- task5/DOM.js
+++ task5/DOM.js
@@ -1,4 +1,4 @@
-﻿(function() {
+(function() {
     var user = null;
     var photoPosts = [{
         id: '1',
@@ -88,7 +88,7 @@
         photoLink: 'site/photo.jpg',
         hashTags: ['#hashTag1', '#hashTag2', '#hashTag3'],
         likes: ['like2', 'like1', 'like3']
-    },{
+    }, {
         id: '12',
         description: 'description12',
         createdAt: new Date('2018-02-21T10:00:00'),
@@ -161,64 +161,53 @@
         hashTags: ['#hashTag1', '#hashTag2', '#hashTag3'],
         likes: ['like2', 'like1', 'like3']
     }];
-    function getPhotoPost(id) {
-        for (var index = 0; index < photoPosts.length; index++) {
-            if (photoPosts[index].id === id) {
-                return photoPosts[index];
+    function editPhotoPost(id, photoPost) {
+        var index = photoPosts.findIndex(function(obj) {
+            return obj.id === id
+        });
+        if (index !== -1) {
+            if (photoPost.description !== undefined && photoPost.description.length !== 0) {
+                document.getElementsByClassName("description_text")[photoPosts.length - index - 1].textContent = photoPost.description;
+                photoPosts[photoPosts.length - index - 1].description = photoPost.description;
             }
-        }
-    }
-    function editPhotoPost(id, photoPost){
-        for(var index = 0; index < photoPosts.length; index++) {
-            if(photoPosts[index].id === id) {
-                if (photoPost.description !== undefined && photoPost.description.length !== 0) {
-                    var editDescription = document.getElementsByClassName("description_text")[photoPosts.length - index - 1];
-                    editDescription.innerHTML = photoPost.description;
-                    photoPosts[photoPosts.length - index - 1].description = photoPost.description;
-                }
-                if (photoPost.hashTags !== undefined && photoPost.hashTags.length !== 0) {
-                    var editHashTags = document.getElementsByClassName("hashTag_text")[photoPosts.length - index - 1];
-                    editHashTags.innerHTML = photoPost.hashTags;
-                    photoPosts[photoPosts.length - index - 1].hashTags = photoPost.hashTags;
-                }
-                if (photoPost.likes !== undefined && photoPost.likes.length !== 0) {
-                    var editLikes = document.getElementsByClassName("likes_text")[photoPosts.length - index - 1];
-                    editLikes.innerHTML = photoPost.likes;
-                    photoPosts[photoPosts.length - index - 1].likes = photoPost.likes;
-                }
-                if (photoPost.photoLink !== undefined && photoPost.photoLink.length !== 0) {
-                    var editPhotoLink = document.getElementsByClassName("photo")[photoPosts.length - index - 1];
-                    editPhotoLink.src = photoPost.photoLink;
-                    photoPosts[photoPosts.length - index - 1].photoLink = photoPost.photoLink;
-                }
+            if (photoPost.hashTags !== undefined && photoPost.hashTags.length !== 0) {
+                document.getElementsByClassName("hashTag_text")[photoPosts.length - index - 1].textContent = photoPost.hashTags;
+                photoPosts[photoPosts.length - index - 1].hashTags = photoPost.hashTags;
+            }
+            if (photoPost.likes !== undefined && photoPost.likes.length !== 0) {
+                document.getElementsByClassName("likes_text")[photoPosts.length - index - 1].textContent = photoPost.likes;
+                photoPosts[photoPosts.length - index - 1].likes = photoPost.likes;
+            }
+            if (photoPost.photoLink !== undefined && photoPost.photoLink.length !== 0) {
+                document.getElementsByClassName("photo")[photoPosts.length - index - 1].src = photoPost.photoLink;
+                photoPosts[photoPosts.length - index - 1].photoLink = photoPost.photoLink;
             }
         }
     }
 
-function displayPhotoposts() {
+    function displayPhotoposts() {
 
         var COUNT_ELEMENTS = 20;
-    for(var index = 0; index < COUNT_ELEMENTS; index++){
-        addPhotoPost(photoPosts[index]);
+        for (var index = 0; index < COUNT_ELEMENTS; index++) {
+            addPhotoPost(photoPosts[index]);
+        }
+        if (user === null) {
+            var userPanel = document.querySelector(".user");
+            userPanel.parentNode.removeChild(userPanel);
+            document.querySelector(".left_header").removeChild(document.querySelector(".button_add_post"));
+            document.querySelector(".button_exit").textContent = 'Авторизация';
+        }
     }
-    if(user === null){
-        var userPanel = document.querySelector(".user");
-        userPanel.parentNode.removeChild(userPanel);
-        document.querySelector(".left_header").removeChild(document.querySelector(".button_add_post"));
-        var buttonExit = document.querySelector(".button_exit");
-        buttonExit.innerHTML = 'Авторизация';
-    }
-}
-function addOptionToSelect(elementArr, select) {
+    function addOptionToSelect(elementArr, select) {
         var option = document.createElement("option");
         option.innerHTML = elementArr;
         select.appendChild(option)
-}
-    function addComment(comment, listComments){
+    }
+    function addComment(comment, listComments) {
         var comment2 = comment.cloneNode(true);
         listComments.appendChild(comment2);
     }
-    function removePhotoPost(id){
+    function removePhotoPost(id) {
         for (var index = 0; index < photoPosts.length; index++) {
             if (photoPosts[index].id === id) {
                 var posts = document.querySelector(".all_posts");
@@ -229,80 +218,47 @@ function addOptionToSelect(elementArr, select) {
             }
         }
     }
-    function hashTagsSelect(){
+    function hashTagsSelect() {
         var arrHashTag = [];
-        for(var index = 0; index < photoPosts.length; index++){
-            for(var item = 0; item < photoPosts[index].hashTags.length; item++){
-                if(arrHashTag.indexOf(photoPosts[index].hashTags[item]) === -1){
+        for (var index = 0; index < photoPosts.length; index++) {
+            for (var item = 0; item < photoPosts[index].hashTags.length; item++) {
+                if (arrHashTag.indexOf(photoPosts[index].hashTags[item]) === -1) {
                     arrHashTag.push(photoPosts[index].hashTags[item]);
                 }
             }
         }
         var select = document.querySelector(".select_text");
-        for(index = 0; index < arrHashTag.length; index++){
-           addOptionToSelect(arrHashTag[index], select);
+        for (index = 0; index < arrHashTag.length; index++) {
+            addOptionToSelect(arrHashTag[index], select);
         }
     }
-function addPhotoPost(photoPost) {
+    function addPhotoPost(photoPost) {
         var posts = document.querySelector('.all_posts');
         var newElem = document.createElement("div");
 
         newElem.className = "post";
         var comment = document.createElement("div");
         comment.className = "comments_text";
-        comment.innerHTML = '<p class="user_name_comments">' +
-            '<img class="user_comments_photo" src="site/user.png"/>Имя пользователя</p> ' +
-            '<p class ="comment">Комментарий</p>';
+        comment.innerHTML = '<p class="user_name_comments">' + '<img class="user_comments_photo" src="site/user.png"/>Имя пользователя</p> ' + '<p class ="comment">Комментарий</p>';
 
-        newElem.innerHTML = '<div class="user_panel">' +
-            '\<div>' +
-            '\<p class="user_name_text_post"> ' +
-            '<img class="user_name_photo_post" src="site/user.png" />' + photoPost.author + '</p>' +
-            '</div>' +
-            '<div class="buttons">' +
-            '\<a class="link_edit_delete" ><img class="button_edit" src="site/delete.png" /></a>' +
-            '<a class="link_edit_like"  ><img class="button_edit_like" src="site/like4.png" /></a>' +
-            '<a class="link_edit"  ><img class="button_edit" src="site/edit.png"/></a>' +
-            '</div>' +
-            '</div>' +
-            '<div class="photopost">' +
-            '\<div class="user_photopost">' +
-            '<p class="date_post">' + photoPost.createdAt + '</p>' +
-            '<div class="description">' +
-            '\<p class="description_text">' + photoPost.description + '</p><p class="hashTag_text">' + photoPost.hashTags + '</p><p class="likes_text">' + photoPost.likes + '</p>' +
-            '</div>' +
-
-            '</div>' +
-            '<div class="comments">' +
-            '\<div class="comments_overflow"> ' +
-            '\<div id="list_comments">' +
-            '</div>' +
-            '</div>' +
-            '<input type="text" class="text" placeholder="Написать коментарий..." />' +
-            '</div>' +
-            '</div>';
-        photo = document.createElement("img");
-        photo.className = "photo";
-        photo.src = photoPost.photoLink;
+        newElem.innerHTML = '<div class="user_panel">' + '\<div>' + '\<p class="user_name_text_post"> ' + '<img class="user_name_photo_post" src="site/user.png" />' + photoPost.author + '</p>' + '</div>' + '<div class="buttons">' + '\<a class="link_edit_delete" ><img class="button_edit" src="site/delete.png" /></a>' + '<a class="link_edit_like"  ><img class="button_edit_like" src="site/like4.png" /></a>' + '<a class="link_edit"  ><img class="button_edit" src="site/edit.png"/></a>' + '</div>' + '</div>' + '<div class="photopost">' + '\<div class="user_photopost">' + '<img class="photo" src="' + photoPost.photoLink + '"><p class="date_post">' + photoPost.createdAt + '</p>' + '<div class="description">' + '\<p class="description_text">' + photoPost.description + '</p><p class="hashTag_text">' + photoPost.hashTags + '</p><p class="likes_text">' + photoPost.likes + '</p>' + '</div>' +
+            '</div>' + '<div class="comments">' + '\<div class="comments_overflow"> ' + '\<div id="list_comments">' + '</div>' + '</div>' + '<input type="text" class="text" placeholder="Написать коментарий..." />' + '</div>' + '</div>';
         var firstElem = document.getElementsByClassName("post")[0];
         posts.insertBefore(newElem, firstElem);
         var listComments = document.getElementById('list_comments');
         listComments.appendChild(comment);
         addComment(comment, listComments);
         addComment(comment, listComments);
-        var firstElemPhoto = document.querySelector(".date_post");
-        var userPhotopost = document.querySelector(".user_photopost");
-        userPhotopost.insertBefore(photo, firstElemPhoto);
-        if(photoPost.id > photoPosts.length) {
+        if (photoPost.id > photoPosts.length) {
             photoPosts.push(photoPost);
         }
-    if(user === null) {
-        var buttons = document.querySelector(".buttons");
-        buttons.removeChild(document.querySelector(".link_edit_delete"));
-        buttons.removeChild(document.querySelector(".link_edit"));
-        document.querySelector(".comments").removeChild(document.querySelector(".text"));
+        if (user === null) {
+            var buttons = document.querySelector(".buttons");
+            buttons.removeChild(document.querySelector(".link_edit_delete"));
+            buttons.removeChild(document.querySelector(".link_edit"));
+            document.querySelector(".comments").removeChild(document.querySelector(".text"));
+        }
     }
-}
 
     console.log(displayPhotoposts());
     console.log(removePhotoPost("3"));
@@ -315,10 +271,12 @@ function addPhotoPost(photoPost) {
         hashTags: ['#roses', '#flowers', '#plant'],
         likes: ['like2', 'like1', 'like3']
     }));
-    console.log(editPhotoPost("1", {description: 'description12',
+    console.log(editPhotoPost("1", {
+        description: 'description12',
         author: 'Author12',
         photoLink: 'site/photo.jpg',
         hashTags: ['#hashTag10', '#hashTag20', '#hashTag30'],
-        likes: ['like20', 'like10', 'like30']}));
+        likes: ['like20', 'like10', 'like30']
+    }));
     console.log(hashTagsSelect());
 }())
